f4daf2a2278622cf999e21fdf930fb419944f0ab
Shivansh Soni <shivanshmsoni@gmail.com>
2024-09-10 14:57:30 -0700

    updates

--- utils/ocr.py
+++ utils/ocr.py
@@ -1,7 +1,22 @@
 import cv2
 import pytesseract
+from wand.image import Image
+from wand.color import Color
 
-img = cv2.imread("assets/stock_sheet.jpg")
+
+input_image_path = "assets/stock_sheet.jpg"
+
+with Image(filename=input_image_path) as img:
+    # Add a white border
+    img.border(color=Color('white'), width=10, height=10)
+
+    # Save the modified image
+    new_filename = str(input_image_path.split('.')[0]) + "_border.jpg"
+    img.save(filename=new_filename)
+
+print("Image processing complete.")
+
+img = cv2.imread(new_filename)
 config = r"--oem 3 --psm 6"
 
 tess_output = pytesseract.image_to_string(img, config=config)
